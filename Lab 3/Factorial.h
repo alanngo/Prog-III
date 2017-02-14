2bc8b5b7a852b328652317e86d47551aa0e2f451
Alexander Botello <alexander.botello@g.austincc.edu>
2017-02-13 22:46:09 -0600

    Add testRun function

--- Lab 3/Factorial.h
+++ Lab 3/Factorial.h
@@ -1,6 +1,8 @@
 #ifndef FACTORIAL_
 #define FACTORIAL_
 
+// Iterative Factorial Function
+// Written by: Alex B
 int factorial(int num)
 {
 	int product = 1;
@@ -11,12 +13,13 @@ int factorial(int num)
 	return product;
 }
 
-
+// Recursive Factorial Function
+// Written by: Alan N
 int recursiveFactorial(int x)
 {
 	if (x == 0)
 		return 1;
-	
+
 	else
 		return x * recursiveFactorial(x - 1);
 }
--- Lab 3/driver.cpp
+++ Lab 3/driver.cpp
@@ -3,36 +3,61 @@
 #include "Factorial.h"
 using namespace std;
 
+// testRun Prototype
+int testRun( int );
 
+// Main Function
 int main()
 {
-	clock_t start;
-	double duration, duration2;
-	int fact,
-	    trialRuns = 150000000;
+	// Test 10,000,000 trial runs
+	testRun(10000000);
 
-	// Test Iterative Solution
-	int n = 12;
-	start = clock();
+	// Test 100,000,000 trial runs
+	testRun(100000000);
 
-	for (int i = 0; i < trialRuns; i++)
-	{
-		fact = factorial(n);
-	}
-	duration = (clock() - double(start)) / CLOCKS_PER_SEC;
-	
-	// Test Recursive Solution
-	start = clock();
-	
-	for (int i = 0; i < trialRuns; i++)
-	{
-		fact = recursiveFactorial(n);
-	}
-	duration2 = (clock() - double(start)) / CLOCKS_PER_SEC;
-	
-	// Display Timing Results
-	cout << "Trial Runs: " << trialRuns << endl;
-	cout << "ITERATIVE SOLUTION: " << duration << " secs" << endl;
-	cout << "RECURSIVE SOLUTION: " << duration2 << " secs" << endl;
+	// Test 300,000,000 trial runs
+	testRun(300000000);
+
+	// Test 1,000,000,000 trial runs
+	testRun(1000000000);
+
 	return 0;
+}
+
+// Test Function
+int testRun(int num)
+{
+		clock_t start;
+		double duration, duration2;
+		int fact,
+		    trialRuns = num;
+
+    // We take the factorial of 12 to be our constant as 12! is the highest
+		// limit the `int` data type can accept. Any number higher will cause
+		// memory overflow issues
+		int n = 12;
+
+		// Test Iterative Solution
+		start = clock();
+
+		for (int i = 0; i < trialRuns; i++)
+		{
+			fact = factorial(n);
+		}
+		duration = (clock() - double(start)) / CLOCKS_PER_SEC;
+
+		// Test Recursive Solution
+		start = clock();
+
+		for (int i = 0; i < trialRuns; i++)
+		{
+			fact = recursiveFactorial(n);
+		}
+		duration2 = (clock() - double(start)) / CLOCKS_PER_SEC;
+
+		// Display Timing Results
+		cout << endl << "Trial Runs: " << trialRuns << endl << endl;
+		cout << "ITERATIVE SOLUTION: " << duration << " secs" << endl;
+		cout << "RECURSIVE SOLUTION: " << duration2 << " secs" << endl;
+		return 0;
 }
